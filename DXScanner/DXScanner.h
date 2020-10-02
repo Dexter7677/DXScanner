6cf2314db800d67e54c781e10216f57169fc8e0f
Dexter <deepak.inbox229@gmail.com>
2020-10-02 05:57:45 +0530

    Initial Commit

---
+++ DXScanner/DXScanner.h
@@ -0,0 +1,18 @@
+//
+//  DXScanner.h
+//  DXScanner
+//
+//  Created by Deepak Singh on 02/10/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DXScanner.
+FOUNDATION_EXPORT double DXScannerVersionNumber;
+
+//! Project version string for DXScanner.
+FOUNDATION_EXPORT const unsigned char DXScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DXScanner/PublicHeader.h>
+
+
