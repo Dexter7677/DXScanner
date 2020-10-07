69c381c0d4aae34986c2bebb30033667aab39627
Dexter <deepak.inbox229@gmail.com>
2020-10-07 05:36:27 +0530

    Added permission request

---
+++ DXScanner/DXScanner.swift
@@ -0,0 +1,54 @@
+//
+//  DXScanner.swift
+//  DXScanner
+//
+//  Created by Deepak Singh on 02/10/20.
+//
+
+import Foundation
+import AVFoundation
+
+public class DXScanner{
+    
+    public func scan(){
+        //open camera for scanning
+        print("Sacanning Called")
+        isAuthorised { (status) in
+            //startScan
+            print("status: \(status)")
+        }
+        
+        
+    }
+    
+    private func isAuthorised( completion:@escaping (Bool) -> Void){
+        switch AVCaptureDevice.authorizationStatus(for: .video){
+        case .notDetermined:
+            AVCaptureDevice.requestAccess(for: .video) { (granted) in
+                if granted{
+                    completion(true)
+                }else{
+                    completion(false)
+                }
+            }
+            break
+        case .restricted:
+            completion(false)
+            break
+        case .denied:
+            completion(false)
+            break
+        case .authorized:
+            completion(true)
+            break
+        @unknown default:
+            completion(true)
+            break
+        }
+    }
+    
+    public init(){
+        
+    }
+}
+
